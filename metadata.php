bffced54cc33dae931a7dad2a6d470191fc2e00b
fotorabe <vigur@gmx.de>
2014-10-01 22:30:56 +0200

    metadata.php

---
+++ metadata.php
@@ -0,0 +1,18 @@
+<?php
+$sMetaDataVersion = "1.1";
+
+$aModule = array
+(
+	'id' 	=> 			'gbxGeburtstagsSonderpreis', 	
+	'title' => 			'Geburtstagssonderpreis-Modul',
+	'description' =>	
+		array 
+		(
+			'de' => 'Sendet eine Email mit Sonderpreisen zur allen Nutzern die am aktuellen Tag Geburtstag haben.',
+			'en' => 'Sends an Email with special prices to all User, which have birthday.'		
+		),
+	'version' => '1.0',
+	'author'  => 'René O.',
+	'extend' => array()
+	
+);
